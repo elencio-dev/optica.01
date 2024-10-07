6a683d7f37ce106fa1d6e7ff6a24bec8f29a0cc6
Elencio <elenciocalado@gmail.com>
2024-10-07 12:04:31 -0300

    fix problemas de eslint no Edital

--- app/Posts/[slug]/page.tsx
+++ app/Posts/[slug]/page.tsx
@@ -1,34 +1,34 @@
-import { getPostBySlug } from "@/lib/getPostBySlug";
-import PostPage from "./PostPage";
+import { getPostBySlug } from '@/lib/getPostBySlug'
+import PostPage from './PostPage'
 
 export const generateMetadata = async ({
   params,
 }: {
-  params: { slug: string };
+  params: { slug: string }
 }) => {
-  const post = await getPostBySlug(params.slug);
+  const post = await getPostBySlug(params.slug)
 
   if (!post) {
     return {
-      title: "Postagem não encontrada",
-      description: "Esta postagem não foi encontrada.",
-    };
+      title: 'Postagem não encontrada',
+      description: 'Esta postagem não foi encontrada.',
+    }
   }
 
   return {
     title: post.title,
-    description: post.content.markdown?.slice(0, 150) ?? "Postagem detalhada",
+    description: post.content.markdown?.slice(0, 150) ?? 'Postagem detalhada',
     openGraph: {
       title: post.title,
-      description: post.content.markdown?.slice(0, 150) ?? "Postagem detalhada",
+      description: post.content.markdown?.slice(0, 150) ?? 'Postagem detalhada',
       url: `https://www.unilabstudentchapter.org/posts/${params.slug}`,
     },
     twitter: {
-      card: "summary_large_image",
+      card: 'summary_large_image',
     },
-  };
-};
+  }
+}
 
 export default function Page({ params }: { params: { slug: string } }) {
-  return <PostPage params={params} />;
+  return <PostPage params={params} />
 }
