a99294e4aad01051da840130ac32497a48a32060
Elencio <elenciocalado@gmail.com>
2024-10-08 09:49:57 -0300

    Alterando a config de imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'media.graphassets.com',
-                port: '', 
-                pathname: '/**', 
-            },
-        ],
-    },
-    async rewrites() {
-        return [
-            {
-                source: "/sitemap.xml",
-                destination: "/sitemap.xml/route",
-            },
-        ];
-    },
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.graphassets.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/sitemap.xml/route',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
